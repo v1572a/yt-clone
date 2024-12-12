17b977003e23d33bce8c628b3c08a65f2605b4a5
Vamshidhar <vamshi16n@gmail.com>
2024-12-12 19:20:38 +0530

    added app.js configuration for cors and added utils higher order func

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import dotenv from 'dotenv';
-import express from 'express';
 import ConnectDB from './db/index.js';
-const app=express();
+import app from './app.js';
+
 dotenv.config();
-ConnectDB();
-// ; (async () =>{
-//    try {
-//      await mongoose.connect(`${process.env.MONGODB_URL}`)
-//      app.on("error",(error)=>{
-//        console.error("Error: ",error)
-//      })
-//      app.listen(process.env.PORT,()=>{
-//         console.log(`server is running on port ${process.env.PORT}`)
-//     })
-//    } catch (error) {
-//     console.error("Error: ",error);
-//     throw error
-//    }
-// })();
+
+ConnectDB()
+.then(()=>{
+    app.listen(process.env.PORT||4000,()=>{
+        console.log(`Server is connected to port ${process.env.PORT}`);
+        
+    })
+})
+.catch()
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,12 @@
+
+const asyncHandler=(func)=>async (req,res,next)=>{
+try {
+await func(req,res,next)  
+} catch (error) {
+    res.status(err.code|| 500).json({
+        success:false,
+        message:err.message
+    })
+}
+}// try catch block
+export {asyncHandler};
