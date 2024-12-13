daa71c361328959a9a89981bbf3a2596292e3f56
Vamshidhar <vamshi16n@gmail.com>
2024-12-13 19:08:59 +0530

    added bcrypt hashing and mongoose methods for creating jwt tokens i.e. refresh and access tokens

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,9 @@
 import mongoose,{Schema} from "mongoose";
+import bcrypt from 'bcrypt';
+import jsonwebtoken from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const userSchema= new Schema({
     username:{
@@ -42,5 +47,42 @@ const userSchema= new Schema({
     }
 },{timestamps:true});
 
+userSchema.pre("save",async function (next){
+    if (this.isModified("password")) {
+        this.password= await bcrypt.hash(this.password,10)
+    }
+next();
+})// no arrow function
+
+userSchema.methods.isPasswordCorrect=async function
+(password)
+{
+ return await bcrypt.compare(password,this.password)
+}
+userSchema.methods.generateAccessToken=function(){
+  return  jsonwebtoken.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullname     
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken=function(){
+    return  jsonwebtoken.sign(
+        {
+            _id:this._id,  
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export  const User=mongoose.model("User",userSchema);
