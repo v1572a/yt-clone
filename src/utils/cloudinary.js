83dcf2eccc985d29fa1d5c304e2af72012966eaf
Vamshidhar <vamshi16n@gmail.com>
2024-12-14 19:54:45 +0530

    Added util Cloudinary.js for file upload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,35 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+import { v2 as cloudinary } from 'cloudinary';
+
+  // Configuration
+  cloudinary.config({ 
+    cloud_name: `${process.env.CLOUDINARY_API_NAME}`, 
+    api_key: `${process.env.CLOUDINARY_API_KEY}`, 
+    api_secret: `${process.env.CLOUDINARY_API_SECRET}`
+});   
+
+const uploadFileCloudinary=async (localFilePath)=>{
+    try {
+        if (!localFilePath) {
+            return null
+        }
+      const response=  cloudinary.uploader.upload(localFilePath,
+            {
+                resource_type:'auto'
+            }
+        )
+        console.log("File uploaded");
+        return response
+        
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        // it removes the file saved if an error occured from the server
+    }
+}
+export {uploadFileCloudinary} ;
+  
+
+
+
